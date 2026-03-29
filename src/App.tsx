c1518d37342536d6ae93359191142ba4f8c29c8b
Bhupendra-glitch <bhupendrasahu132004@gmail.com>
2026-03-29 10:51:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -244,7 +244,7 @@ export default function App() {
               whileTap={{ scale: 0.95 }}
               className="bg-black text-[#E4E3E0] px-8 py-4 flex items-center gap-3 font-mono text-xs uppercase tracking-widest hover:bg-[#F27D26] transition-colors"
             >
-              Download Resume < e:\MY Resume\New Resume 3.pdf />
+              Download Resume <"E:\MY Resume\New Resume 3.pdf"/>
             </motion.button>
           </div>
         </div>
